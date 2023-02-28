af444ba5a64d89083faab1bce7650840a1016358
Leoc0536 <113024598+Leoc0536@users.noreply.github.com>
2023-02-28 11:28:33 +0800

    Add files via upload

---
+++ SchoolManager.java
@@ -0,0 +1,11 @@
+/*  Course Name: HDDS1103 Object-oriented Programming
+    Student Name: Chan Man Chun (Leo), Chan Kwing Tung Jevons, Lo Tze Kit (Kit), To Ho Hin (Herman)
+    Student ID: 22661204, 22661077, 22661085, 22661026
+*/
+
+public class SchoolManager {
+    public static void main(String[] args) {
+        System.out.println("Hello World.");
+    }
+}
+
