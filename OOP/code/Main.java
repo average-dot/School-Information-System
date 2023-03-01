580af89fd064035594181190ca856b55dfab07aa
Leoc0536 <113024598+Leoc0536@users.noreply.github.com>
2023-03-01 21:19:25 +0800

    Add files via upload

---
+++ OOP/code/Main.java
@@ -0,0 +1,19 @@
+/*  Course Name: HDDS1103 Object-oriented Programming
+    Student Name: Chan Man Chun (Leo), Chan Kwing Tung Jevons, Lo Tze Kit (Kit), To Ho Hin (Herman)
+    Student ID: 22661204, 22661077, 22661085, 22661026
+*/
+package code;
+abstract class User{
+    int userID;
+    int name;
+    int DOB;
+    int HKID;
+    void getName(){}
+    void getUserId(){}
+}
+
+// Student
+// parent
+// teacher
+
+// read Attendance
---
+++ OOP/code/SchoolManager.java
@@ -0,0 +1,15 @@
+package code;
+/*  Course Name: HDDS1103 Object-oriented Programming
+    Student Name: Chan Man Chun (Leo), Chan Kwing Tung Jevons, Lo Tze Kit (Kit), To Ho Hin (Herman)
+    Student ID: 22661204, 22661077, 22661085, 22661026
+*/
+
+class SchoolManager {
+    public String toString() {
+        return "Hello World 123";
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello World.");
+    }
+}
+
